8475cf0e02157c25e86add87db09daab852859b3
mbasart <mbd2797@gmail.com>
2020-12-07 20:20:06 +0100

    some changes in the main.cpp

--- injectDllAllProcess/main.cpp
+++ injectDllAllProcess/main.cpp
@@ -11,54 +11,6 @@
 #include <exception>
 using namespace std;
 
-// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
-// and compile with -DPSAPI_VERSION=1
-
-/*
-int PrintProcessNameAndID(DWORD processID)
-{
-    TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-    int valueProc = 0; //si es unknow retorna 0, know 1!!!!!!!!!!!!!!
-
-    // Get a handle to the process.
-
-    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
-        PROCESS_VM_READ,
-        FALSE, processID);
-
-    // Get the process name.
-
-    if (NULL != hProcess)
-    {
-        HMODULE hMod;
-        DWORD cbNeeded;
-
-        if (EnumProcessModules(hProcess, &hMod, sizeof(hMod),
-            &cbNeeded))
-        {
-            GetModuleBaseName(hProcess, hMod, szProcessName,
-                sizeof(szProcessName) / sizeof(TCHAR));
-        }
-    }
-
-    // Print the process name and identifier. 
-
-    _tprintf(TEXT("%s  (PID: %u)\n"), szProcessName, processID);
-
-    if (wcscmp(szProcessName, TEXT("<unknown>")) == 0)
-    { 
-        valueProc = 0;
-        printf("Unknown\n");
-    }
-    else
-        valueProc = 1;
-
-    // Release the handle to the process.
-
-    CloseHandle(hProcess);
-    return valueProc;
-}*/
-
 int main(void)
 {
     // Get the list of process identifiers.
@@ -67,8 +19,6 @@ int main(void)
     unsigned int i;
 
     const char* dllPath = "C:\\Users\\User\\Desktop\\MALW2\\hook64v3.dll";
-    //const char* procName = "csgo.exe";
-    //DWORD procId = 0;
 
     if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
     {
@@ -86,17 +36,7 @@ int main(void)
     {
         if (aProcesses[i] != 0)
         {
-            //int resultValue = PrintProcessNameAndID(aProcesses[i]);
             //_tprintf(TEXT("  (PID: %u)\n"), aProcesses[i]);
-
-            //procId = aProcesses[i];
-
-            /*while (!procId)
-            {
-                procId = GetProcId(procName);
-                Sleep(30);
-            }*/
-
             
             try{
                 HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, aProcesses[i]);
